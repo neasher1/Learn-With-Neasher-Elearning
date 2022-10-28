021acea1bcdac61a92b2136a82315276211b4fd4
neasher <neasherahmed75@gmail.com>
2022-10-28 23:12:10 +0600

    Course fixed

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -9,10 +9,10 @@ const Home = () => {
                 <div className="row align-items-center">
                     <div className="col-lg-12">
                         <h1 className='text-white py-5'>LEARN SOMETHING NEW WITH BETTER QUALITY</h1>
-                        <h4 className='text-white py-5'>Versatile Training Programs to Help You Achieve Your Goals. Learn More Details Today. We Offer <br /> Comprehensive Training Programs Suited to Your Needs. Learn More. For the Vision Impaired. With Industry Employment. <br /> Online Technical Courses. Opportunities-Call today.</h4>
-                       <div>
-                        <button className='btn-course py-3 my-3'><Link to='/courses' className='mx-2' style={{'textDecoration':'none' , 'color':'white'}}>Browse Courses</Link><FaArrowRight/></button>
-                       </div>
+                        <h4 className='text-white py-5'>Learn With Neasher Programs to Help You Achieve Your Goals. Learn More Details Today. We Offer <br /> Comprehensive Training Programs Suited to Your Needs. Learn More. For the Vision Impaired. With Industry Employment. <br /> Online Technical Courses. Opportunities-Call today.</h4>
+                        <div>
+                            <button className='btn-course py-3 my-3'><Link to='/courses' className='mx-2' style={{ 'textDecoration': 'none', 'color': 'white' }}>Browse Courses</Link><FaArrowRight /></button>
+                        </div>
                     </div>
                 </div>
             </div>
